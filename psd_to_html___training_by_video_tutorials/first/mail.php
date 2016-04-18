9f8d204345bd3e76a6b931bc1283be6832c4e096
mali3days <mali3days@gmail.com>
2016-04-18 17:08:09 +0300

    some update

--- psd_to_html___training_by_video_tutorials/first/mail.php
+++ psd_to_html___training_by_video_tutorials/first/mail.php
@@ -1,12 +1,11 @@
 <?php
 
-$recepient = "youmail@ya.ru";
-$sitename = "Название сайта";
+$recepient = "mali3days@gmail.com";
+$sitename = "moDesign";
 
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$pagetitle = "Новая заявка с сайта \"$sitename\";
+$message = "Имя: $name \nТелефон: $phone";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
